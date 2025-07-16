832f77d2d64a6a8646fcaa7f32a95406f4c3a4cd
Chris Helming <chris.helming@gmail.com>
2025-07-15 22:12:29 -0400

    trying to fix definition

---
+++ boards/shields/say_gay/say_gay_dongle.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2025
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#pragma once
+
+/* 
+ * Define ZMK_SPLIT_BLE_PERIPHERAL_COUNT for backward compatibility
+ * with the prospector_adapter shield that expects this non-standard name
+ */
+#define ZMK_SPLIT_BLE_PERIPHERAL_COUNT CONFIG_ZMK_SPLIT_BLE_PERIPHERAL_COUNT
